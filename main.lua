7b04837d97d1c287b8bac0ec1bfd7588d45a21a7
MillhioreBT <sarahelizabetwesker@gmail.com>
2023-02-03 23:38:09 -0400

    first files

---
+++ main.lua
@@ -0,0 +1,13 @@
+--[[
+	LuaOTB
+	By: MillhioreBT
+	version: 1.0
+]]--
+
+local otb = OTB()
+otb:load("items.otb")
+
+print(otb.minorVersion)
+print(otb.majorVersion)
+print(otb.buildVersion)
+print(otb.description)
